86bf1d226adcce179d94ebc8bd0c21ba5a63dec1
JetSG <145757524+JetSG@users.noreply.github.com>
2023-10-31 18:43:13 +1100

    fixed

--- MATLAB Testing/@KinovaLink6/KinovaLink6.m
+++ MATLAB Testing/@KinovaLink6/KinovaLink6.m
@@ -16,25 +16,13 @@ classdef KinovaLink6 < RobotBaseClass
 
         function CreateModel(self)
             %Create the Kinova Link 6 model
-<<<<<<< HEAD
             link(1) = Link('d',+0.1375+0.0515, 'a',-0.110244, 'alpha',pi/2, 'offset',-pi/2);
             link(2) = Link('d',0.06925, 'a',0.485, 'alpha',0, 'offset',pi/2);
             link(3) = Link('d',-0.0917, 'a',0, 'alpha',pi/2, 'offset',-pi/2);
-            link(4) = Link('d',-0.23975-0.0917, 'a',0, 'alpha',pi/2, 'offset',pi);
+            link(4) = Link('d',-0.23975-0.0917, 'a',0, 'alpha',pi/2, 'offset',0);
             link(5) = Link('d',+0.078422+0.06296, 'a',-0.086, 'alpha',pi/2, 'offset',pi/2);
             link(6) = Link('d',0.092+0.08708, 'a',0, 'alpha',0, 'offset',0);
-            % link(1) = Link('d',0, 'a',0, 'alpha',0, 'offset',0);
-            % link(2) = Link('d',1, 'a',0, 'alpha',0, 'offset',0);
-=======
-            link(1) = Link('d',0.0515, 'a',0.0, 'alpha',pi, 'offset',0);
-            link(2) = Link('d',-0.1375, 'a',0.11024, 'alpha',-pi/2, 'offset',0); %offset in cad by 69.25 mm along z
-            link(3) = Link('d',0.485, 'a',0, 'alpha',pi, 'offset',0);
-            % link(4) = Link('d',0.15216, 'a',0.09170, 'alpha',0, 'offset',0);
-            % link(5) = Link('d',0.23975, 'a',-0.06296, 'alpha',pi/2, 'offset',0);
-            % link(6) = Link('d',0.086, 'a',-0.07692, 'alpha',0, 'offset',0);
-            %link(1) = Link('d',0, 'a',0, 'alpha',0, 'offset',0);
-            %link(2) = Link('d',0.1375, 'a',0.11024, 'alpha',pi/2, 'offset',0);
->>>>>>> b37769249065c6f042001a1c722979d1ca69aec1
+
             % link(3) = Link('d',0, 'a',0, 'alpha',0, 'offset',0);
             % link(4) = Link('d',0, 'a',0, 'alpha',0, 'offset',0);
             % link(5) = Link('d',0, 'a',0, 'alpha',0, 'offset',0);
