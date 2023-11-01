2d0dd517915c40648f25abe88b15fd9cea69fc38
JetSG <145757524+JetSG@users.noreply.github.com>
2023-11-01 13:57:50 +1100

    kinovatesting

--- MATLAB Testing/testingjoint.m
+++ MATLAB Testing/testingjoint.m
@@ -1,4 +1,17 @@
-q = [0, 0, 0, 0, 0, 0]
 
-x = KinovaLink6(q);% Replace q with your joint angles
-% KinovaGen3();
+x = KinovaLink6();%
+
+figure(); 
+%%
+x.model.plot([ pi/2, 0, 0, 0, 0, 0], 'scale', 0.5, '[no]tiles', '[no]name')
+%%
+x.model.plot([0,  pi/2, 0, 0, 0, 0], 'scale', 0.5)
+%%
+x.model.plot([0, 0, pi/2, 0, 0, 0], 'scale', 0.5)
+%%
+x.model.plot([0, 0, 0,  pi/2, 0, 0], 'scale', 0.5)
+%%
+x.model.plot([0, 0, 0, 0, pi/2, 0], 'scale', 0.5)
+%%
+x.model.plot([0, 0, 0, 0, 0,  pi/2], 'scale', 0.5)
+%%
